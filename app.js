b26703278b2d8a768a959a4f683a76e16d2a73cd
hadeelabuzayed <hadeelabuzayed01@gmail.com>
2023-08-08 12:49:38 +0000

    all result is done

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 "use strict";
 let userName = prompt('Enter your name');
-console.log(userName);
+console.log(userName)
+
 let mark = prompt('Welcome ' + userName + ', please enter your mark');
 console.log(mark);
-
-if (mark >= 0 && mark <= 49) {
+let mark1 = Math.floor(mark);
+if (mark1 >= 0 && mark1 <= 49) {
     alert( 'Your result is (F) , so you are fail');
 }
-else if (mark >= 50 && mark <= 60) {
+else if (mark1 >= 50 && mark1 <= 60) {
     alert('Your result is(D) , so you are pass');
 }
-else if (mark >= 61 && mark <= 70) {
+else if (mark1 >= 61 && mark1 <= 70) {
     alert('Your result is (C) , so you are pass');
 }
-else if (mark >= 71 && mark <= 80) {
+else if (mark1 >= 71 && mark1 <= 80) {
     alert( 'Your result is (B) , so you are pass');
 }
-else if (mark >= 81 && mark <= 90) {
+else if (mark1 >= 81 && mark1 <= 90) {
     alert('Your result is (A) , so you are pass');
 }
-else if (mark >= 91 && mark <= 100) {
+else if (mark1 >= 91 && mark1 <= 100) {
     alert('Your result is (A+) , so you are pass');
 }
 else {
